73d72ddf7d0ff29680db3373a68a7b91352d8380
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-30 12:09:50 -0300

    Armando la pagina de busqueda

--- App.js
+++ App.js
@@ -3,13 +3,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
 import { TextInput } from 'react-native-web';
 import Formulario from './Formulario';
-import FlatList from './FlatList';
+import Lista from './Lista';
 
 export default function App() {
   return (
     <>
-      <Text>asdasd  </Text>
-      <FlatList/>
+      <Lista/>
       </>
   );
 }
--- Lista.js
+++ Lista.js
@@ -1,9 +1,11 @@
-import {React, useState, useEffect} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
+import { React, useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList, TextInput, Image } from "react-native";
 
 /*
 const [data, setData] = useState([]);
+const [isLoading, setIsLoading] = useState(true)
+
 function postData(nombre) {
   const response =  fetch(`https://api.spoonacular.com/recipes/716429/information?apiKey=fd84a84ef20c45ff89b371786e57ef04&includeNutrition=true/recipes/complexSearch?query=${nombre}number=2.`, {
   method: 'GET',
@@ -11,6 +13,7 @@ function postData(nombre) {
   }).then((response) => response.json())
 .then((responseJson) => {
   setData(responseJson.results);
+  setIsLoading(false)
 })
 .catch((error) => {
   alert(JSON.stringify(error));
@@ -27,51 +30,56 @@ useEffect(()=>{
 
 const DATA = [
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
   },
   {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
   },
 ];
 
-
-
-const Item = ({title}) => (
+const Item = ({ title, image }) => (
   <View style={styles.item}>
+    <Image source={image}/>
     <Text style={styles.title}>{title}</Text>
   </View>
 );
 
-const FlatList = () => {
+const Lista = () => {
   return (
     <View style={styles.container}>
+      <Text>Ingrese el plato que desea buscar</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="ingrese..."
+        onChangeText={useEffect}
+        id='nombre'
+      />
       <FlatList
         data={DATA}
-        renderItem={({item}) => <Item title={item.title} />}
-        keyExtractor={item => item.id}
+        renderItem={({ item }) => <Item title={item.title}  />}
+        keyExtractor={(item) => item.id}
       />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    margin:12,
     flex: 1,
-    backgroundColor: '#34813C',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#34813C",
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: "#f9c2ff",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
@@ -81,4 +89,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default FlatList
+export default Lista;
